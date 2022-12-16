025d658465275f1e21c6788db7de0f1148004fd4
Mahadi Hassan <itsmahadi@gmail.com>
2022-12-16 20:48:12 +0600

    added vue use and icon

--- components/HelloWorld007.vue
+++ components/HelloWorld007.vue
@@ -6,6 +6,8 @@
     <button class="rounded bg-amber-800 text-amber-50" @click="Counter.increment">increment</button>
     <br/>
     <button @click="Counter.decrement">decrement</button>
+    <div>pos: {{ x }}, {{ y }}</div>
+    <Icon name="clarity:calendar-solid-badged"/>
   </div>
 </template>
 
@@ -13,6 +15,8 @@
 import {useCounter} from "~/store/counter";
 
 const Counter = useCounter()
+
+const {x, y} = useMouse()
 
 </script>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // server side rendering mode
-  ssr: true,
-  modules: ["@nuxtjs/tailwindcss",'@pinia/nuxt',],
+    // server side rendering mode
+    ssr: true,
+    modules: [
+        "@nuxtjs/tailwindcss",
+        '@pinia/nuxt',
+        '@vueuse/nuxt',
+        'nuxt-icon'
+    ],
 });
